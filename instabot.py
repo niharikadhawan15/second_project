dbd1cd8102ee6412a59f0471f0f74b8bec3b0fa3
niharikadhawan15 <niharikadhawan15@gmail.com>
2017-04-09 15:47:07 +0530

    instabot.py modified4

--- instabot.py
+++ instabot.py
@@ -10,20 +10,23 @@ app_access_token='4982415356.db0568c.d88e476700ec4d5caf3cb4649b183ad9'
 Base_Url='https://api.instagram.com/v1'
 
 data=requests.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=4982415356.db0568c.d88e476700ec4d5caf3cb4649b183ad9')
-print data.json()
+print "JSON data of the owner is ",data.json()
 
 #This function is used to fetch owner's details
 def owner_info():
     url=Base_Url+'/users/self/?access_token='+app_access_token
     my_info=requests.get(url).json()
-    print "Details of owner:"
-    print "Url is :"
+    print "\nDetails of owner:"
+    print "\nUrl is :"
     print url
-    print "owner's information in json format is:"
+    print "\nowner's information in json format is:"
     print my_info
-    print my_info['data']['website']
-    print my_info['data']['bio']
-    print my_info['data']['full_name']
+    print "\nUsername is  : ", my_info['data']['username']
+    print "\nName of the user is :",my_info['data']['full_name']
+    print "\nThe link to profile picture is : ", my_info['data']['profile_picture']
+    print "\nThe owner is followed By : ", my_info['data']['counts']['followed_by']
+    print "\nThe followers of the owner are : ", my_info['data']['counts']['follows']
+
 owner_info()
 
 #This function is used to get another user Id on which operations such as like,comment are to be performed.
@@ -31,21 +34,22 @@ def user_by_username(insta_user):
     url=Base_Url+'/users/search?q='+insta_user+'&access_token='+app_access_token
     my_info=requests.get(url).json()
     if len(my_info['data']):
+        print "The user id is :\n"
         print my_info
         print my_info['data'][0]['id']
         return my_info['data'][0]['id']
     else:
          print "No such user"
 
-user_by_username('visheshdhawan')
+#user_by_username('visheshdhawan')
 
 #This function is used to fetch user/'s public posts
 def get_user_post(insta_username):
     insta_user_id=user_by_username(insta_username)
     request_url=Base_Url+'/users/'+insta_user_id+'/media/recent/?access_token='+app_access_token
     recent_posts=requests.get(request_url).json()
-    print "number of recent posts : "+ str(len(recent_posts))
-    x=raw_input("Do you want the id of recent posts or any of the public post of the user ? \n \nPress y to get the id of recent posts only.\n Press n to get the id of any public post of the user")
+    print "\nNumber of recent posts : "+ str(len(recent_posts))
+    x=raw_input("\nDo you want the id of recent posts or any of the public post of the user ? \nPress y to get the id of recent posts only.\nPress n to get the id of any public post of the user")
     if x=='y'or x=='Y':
         a = int(raw_input("\nenter post number for which you want to get the id\n"))
         if  len(recent_posts)>a>0:
@@ -63,11 +67,12 @@ def get_user_post(insta_username):
                 a = []
                 for i in (range(len(recent_posts['data']))):
                     a.append(recent_posts['data'][i]['likes']['count'])
+                    print "\nThe numbers of like on posts are"
                     print recent_posts['data'][i]['likes']['count']
                 print a
                 b = max(a)
                 key=a.index(b)
-                print "Maximum likes are " + str(b) + " on recent post no." + str(key)
+                print "\nMaximum likes are " + str(b) + " on recent post no." + str(key)
                 z=key
                 return recent_posts['data'][z]['id']
         else:
@@ -81,6 +86,7 @@ def like_post_for_user(insta_username):
     request_url=Base_Url+"/media/"+str(post_id)+"/likes/?access_token="+app_access_token
     response_to_like=requests.post(request_url).json()
     print response_to_like['meta']['code']
+    print "\n The response to like is\n"
     print response_to_like
     if response_to_like['meta']['code'] ==200:
          print "Like operation is successful"
@@ -98,11 +104,11 @@ def comment_user_post(insta_user):
     payload = {'access_token': app_access_token, 'text':text }
     response = requests.post(url, payload).json()
     if response['meta']['code'] == 200:
-        print("Your comment has been Posted on the particular post_id.")
+        print "Your comment has been Posted on the particular post_id."
     else:
-        print("The comment operation was unsuccessful")
+        print "The comment operation was unsuccessful"
 
-#comment_user_post('api_17790')
+comment_user_post('api_17790')
 
 # This function is used to return comment Id that contains a particular word.
 def search_word_in_comment(insta_user):
@@ -110,19 +116,32 @@ def search_word_in_comment(insta_user):
     request_url = Base_Url + "/media/" + str(post_id) + "/comments/?access_token=" + app_access_token
     response = requests.get(request_url).json()
     print response
-    word = raw_input("Enter the word you want to search in the comments : ")
-    #comments=[]
-    #comments.append(comments[word])
-    #print comments
+    word = raw_input ("\nEnter the word you want to search in the comments : ")
     for i in response['data']:
         if i in response['data']:
-            print "commeni-id is :"
-            print response['data'][1]
-            return response['data'][1]
+            print "comment-id is :"
+            print response['data'][-1]['id']
+            print response['data'][-1]['text']
+            return response['data'][-1]['id']
     else:
         print "No such comment is there that contains this word."
 
-search_word_in_comment('visheshdhawan')
+#search_word_in_comment('visheshdhawan')
 
+def delete_comment(insta_user):
+    post_id=get_user_post(insta_user)
+    print post_id
+    comment_id=search_word_in_comment(insta_user)
+    #a='DELETE'
+    #payload= {'DELETE':a}
+    request_url=Base_Url+'/media/'+post_id+'/comments/'+str(comment_id)+'?access_token='+app_access_token
+    print request_url
+    comments_response=requests.delete(request_url).json()
+    print comments_response
+    if comments_response['meta']['code']==200:
+        print "Comment deleted successfully"
+    else:
+        print "The delete comment operation unsuccessful"
 
+delete_comment('api_17790')
 
